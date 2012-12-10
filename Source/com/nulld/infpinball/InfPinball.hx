4d8a21ab99caeeea49b0b0e2f85a7ff726745b46
nulld <nulldivide@gmail.com>
2012-12-10 20:04:26 +0400

    - project template fixed

---
+++ Source/com/nulld/infpinball/InfPinball.hx
@@ -0,0 +1,50 @@
+package com.nulld.infpinball;
+
+
+import nme.display.Sprite;
+import nme.display.StageAlign;
+import nme.display.StageScaleMode;
+import nme.Lib;
+
+
+/**
+ * @author nulldivide
+ */
+class InfPinball extends Sprite {
+	
+	
+	public function new () {
+		
+		super ();
+		
+		initialize ();
+		
+		
+		
+	}
+	
+	
+	private function initialize ():Void {
+		
+		Lib.current.stage.align = StageAlign.TOP_LEFT;
+		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
+
+		
+	}
+	
+	
+	
+	
+	// Entry point
+	
+	
+	
+	
+	public static function main () {
+		
+		Lib.current.addChild (new InfPinball ());
+		
+	}
+	
+	
+}
